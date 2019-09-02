0cfcbce099b2681b0752d02c15f7215e6feb68a5
Sean C. Sullivan <github@seansullivan.com>
2019-09-02 12:37:52 -0700

    add SmsServletTest

---
+++ src/test/java/io/github/sullis/twilio/example/sms/SmsServletTest.java
@@ -0,0 +1,25 @@
+package io.github.sullis.twilio.example.sms;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SmsServletTest {
+
+    @Test
+    public void happyPath() throws Exception {
+        String taco = "🌮";
+        MockHttpServletRequest req = new MockHttpServletRequest();
+        req.addParameter("From", "555-555-5555");
+        req.addParameter("To", "777-777-7777");
+        req.addParameter("Body", taco);
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        SmsServlet servlet = new SmsServlet();
+        servlet.doPost(req, response);
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?><Response><Message from=\"777-777-7777\" to=\"555-555-5555\"><Media>https://sullis.github.io/noto-emoji-tools/assets/400/emoji_u1f32e.png</Media></Message></Response>\n",
+                response.getContentAsString());
+    }
+}
