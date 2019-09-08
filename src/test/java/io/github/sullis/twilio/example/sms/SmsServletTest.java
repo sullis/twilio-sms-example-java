e8c4aacabc92e5a496dbdcae4872aaf03404bc66
Sean C. Sullivan <github@seansullivan.com>
2019-09-08 11:22:48 -0700

    SmsServletTest: two tacos

--- src/test/java/io/github/sullis/twilio/example/sms/SmsServletTest.java
+++ src/test/java/io/github/sullis/twilio/example/sms/SmsServletTest.java
@@ -11,16 +11,18 @@ public class SmsServletTest {
 
     @Test
     public void happyPath() throws Exception {
-        String taco = "🌮";
+        final String taco = "🌮";
+        final String requestBody = taco + taco;
         MockHttpServletRequest req = new MockHttpServletRequest();
         req.addParameter("From", "555-555-5555");
         req.addParameter("To", "777-777-7777");
-        req.addParameter("Body", taco);
+        req.addParameter("Body", requestBody);
         MockHttpServletResponse response = new MockHttpServletResponse();
         SmsServlet servlet = new SmsServlet();
         servlet.doPost(req, response);
         assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
                 + "<Response><Message from=\"777-777-7777\" to=\"555-555-5555\">"
+                + "<Media>https://sullis.github.io/noto-emoji-tools/assets/400/emoji_u1f32e.png</Media>"
                 + "<Media>https://sullis.github.io/noto-emoji-tools/assets/400/emoji_u1f32e.png</Media>"
                 + "</Message></Response>",
                 response.getContentAsString());
