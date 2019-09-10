1fc27bf5f8b62609581225ec8bb79c9624cc19fb
Sean C. Sullivan <github@seansullivan.com>
2019-09-09 17:50:59 -0700

    refactor [SmsServletTest]

--- src/test/java/io/github/sullis/twilio/example/sms/SmsServletTest.java
+++ src/test/java/io/github/sullis/twilio/example/sms/SmsServletTest.java
@@ -8,11 +8,20 @@ import org.springframework.mock.web.MockHttpServletResponse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SmsServletTest {
+    static private final String taco = "🌮";
 
     @Test
-    public void happyPath() throws Exception {
-        final String taco = "🌮";
-        final String requestBody = taco + taco;
+    public void happyPath_taco() throws Exception {
+        MockHttpServletResponse response = processRequest(taco + " " + taco);
+        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+                        + "<Response><Message from=\"777-777-7777\" to=\"555-555-5555\">"
+                        + "<Media>https://sullis.github.io/noto-emoji-tools/assets/400/emoji_u1f32e.png</Media>"
+                        + "<Media>https://sullis.github.io/noto-emoji-tools/assets/400/emoji_u1f32e.png</Media>"
+                        + "</Message></Response>",
+                response.getContentAsString());
+    }
+
+    private MockHttpServletResponse processRequest(String requestBody) throws Exception {
         MockHttpServletRequest req = new MockHttpServletRequest();
         req.addParameter("From", "555-555-5555");
         req.addParameter("To", "777-777-7777");
@@ -20,11 +29,6 @@ public class SmsServletTest {
         MockHttpServletResponse response = new MockHttpServletResponse();
         SmsServlet servlet = new SmsServlet();
         servlet.doPost(req, response);
-        assertEquals("<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-                + "<Response><Message from=\"777-777-7777\" to=\"555-555-5555\">"
-                + "<Media>https://sullis.github.io/noto-emoji-tools/assets/400/emoji_u1f32e.png</Media>"
-                + "<Media>https://sullis.github.io/noto-emoji-tools/assets/400/emoji_u1f32e.png</Media>"
-                + "</Message></Response>",
-                response.getContentAsString());
+        return response;
     }
 }
