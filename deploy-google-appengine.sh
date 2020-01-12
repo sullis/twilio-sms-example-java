c65f6225699129f594b5b81c094769477330e373
Sean C. Sullivan <github@seansullivan.com>
2020-01-11 21:53:17 -0800

    tweak deploy-google-appengine.sh

--- deploy-google-appengine.sh
+++ deploy-google-appengine.sh
@@ -1,7 +1,7 @@
 if [ -f "~/twilio.properties" ]; then
   cp ~/twilio.properties ./src/main/resources
 fi
-mvn appengine:deploy
+mvn package appengine:deploy
 if [ -f "./src/main/resources/twilio.properties" ]; then
   git checkout ./src/main/resources/twilio.properties
 fi
